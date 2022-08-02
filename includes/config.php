c7ddf468aa3ab710b3d7fe6d516226b0639d4ea8
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-03 00:26:05 +0300

    init: set some db attributes

--- includes/config.php
+++ includes/config.php
@@ -5,4 +5,11 @@
 
     $db = new PDO('mysql:host=127.0.0.1; dbname='.$db_name.';charset=utf8', $db_user, $db_password);
 
+    //set some db attributes
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    //this make db connection faster
+    $db->setAttribute(PDO::MYSQL_ATTR_USED_BUFFERED_QUERY, true);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    define('APP_NAME', 'PHP REST API TUTORIAL')
 ?>
