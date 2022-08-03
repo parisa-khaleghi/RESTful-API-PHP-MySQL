6d71827c771b6c61b4d1b30d730c5dbf11524cdd
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-03 14:54:05 +0300

    Feat: initializing our api

--- api/read.php
+++ api/read.php
@@ -3,4 +3,8 @@
     // use * to allowing anyone without the problem about authentication 
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application.json');
+
+    //initializing our api
+    //to load our pathes
+    include_once('../core/initialize.php');
 ?>
