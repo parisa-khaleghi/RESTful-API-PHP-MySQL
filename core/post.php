96d7282d9416cb9c42a25d1fd9d3ea98b0afc37c
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-03 14:34:44 +0300

    Feat: add post class to read data from post table

---
+++ core/post.php
@@ -0,0 +1,46 @@
+<?php
+
+    class Post{
+        //sb stuff
+        private $conn;
+        private $table = 'posts';
+
+        //post properties
+        public $id;
+        public $category_id;
+        public $category_name;
+        public $title;
+        public $body;
+        public $author;
+        public $create_at;
+
+        // intitialize this class or particular class
+        //constructor with db connection
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        //getting posts from our database
+        public function read(){
+            //create query
+            $query = 'SELECT c.name as category_name
+                             p.id,
+                             p.category_id,
+                             p.title,
+                             p.body,
+                             p.author,
+                             p.create_at 
+                        FROM '.$this->$table.' p LEFT JOIN categories c
+                          ON p.category_id = c.id
+                  ORDERED BY p.created_at DESC';
+
+            //prepare statement
+            $stmt = $this->$conn->prepare($query);
+
+            //execute query
+            $stmt=>execute();
+
+            return $stmt;
+        }
+    }
+?>
