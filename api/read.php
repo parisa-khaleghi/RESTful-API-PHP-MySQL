23d70bbdbf6804cd9731ae39867e22aedb3c0c3a
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-03 14:53:40 +0300

    Feat: Add headers

---
+++ api/read.php
@@ -0,0 +1,6 @@
+<?php
+    //headers
+    // use * to allowing anyone without the problem about authentication 
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application.json');
+?>
