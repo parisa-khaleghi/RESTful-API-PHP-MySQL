ac4b10edd9b6b93c74aa7f7b736792166ff98e45
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-03 16:22:28 +0300

    Fix: change create to created in query

--- core/post.php
+++ core/post.php
@@ -29,7 +29,7 @@
                              p.title,
                              p.body,
                              p.author,
-                             p.create_at 
+                             p.created_at 
                         FROM '.$this->$table.' p LEFT JOIN categories c
                           ON p.category_id = c.id
                   ORDER BY p.created_at DESC';
