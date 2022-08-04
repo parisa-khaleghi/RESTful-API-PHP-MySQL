0ed0f657d5cc5e0a57596a397e345be4e39c7220
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 19:33:33 +0300

    Feat: create functions to connection with DB

---
+++ src/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Database
+    {
+        private string $host;
+        private string $name;
+        private string $user;
+        private string $password;
+
+        public function __construct(string $host, string $name, string $user, string $password)
+        {
+            $this->host = $host;
+            $this->name = $name;
+            $this->user = $user;
+            $this->password = $password;
+        }
+
+        public function get_connection() : PDO
+        {
+            $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+
+            return new PDO($dsn, $this->user, $this->password);
+        }
+    }
+?>
