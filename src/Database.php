50dbd8aae36cf7a4f96e300f5878cee8eb94b0a1
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 20:29:37 +0300

    Feat: stop showing number as string

--- src/Database.php
+++ src/Database.php
@@ -19,7 +19,10 @@
         {
             $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
 
-            return new PDO($dsn, $this->user, $this->password);
+            return new PDO($dsn, $this->user, $this->password, [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+            ]);
         }
     }
 ?>
