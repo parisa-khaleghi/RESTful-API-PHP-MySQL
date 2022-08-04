0a1097061ab166465a4dd0c8d3d1df878542b94b
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 18:36:14 +0300

    Feat: add setting method controllers

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
+    declare(strict_types=1);
+
+    spl_autoload_register(function($class){
+        require __DIR__ . "/src/$class.php";
+    });
 
     $parts = explode("/", $_SERVER["REQUEST_URI"]);
-    
-    print_r($parts);
+
+    if($parts[2] != "post"){
+        http_response_code(404);
+        exit;
+    }
+
+    $id = $parts[3] ?? null;
+
+    $controller = new Post();
+    $controller->process_request($_SERVER["REQUEST_METHOD"], $id);
 ?>
