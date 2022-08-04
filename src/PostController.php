6855e8df112d1f699faa5db7500b95047f3fc73a
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 20:18:20 +0300

    Feat: call get_all function

--- src/PostController.php
+++ src/PostController.php
@@ -2,6 +2,11 @@
 
     class PostController{
 
+        private ProductGateway $gateway;
+        public function __construct(ProductGateway $gateway){
+            $this->gateway = $gateway;
+        }
+
         public function process_request(string $method, ? string $id) : void{
             if($id){
                 $this->process_resource_request($method, $id);
@@ -17,7 +22,7 @@
         public function process_collection_request(string $method) : void{
             switch($method){
                 case "GET":
-                    echo json_encode(["id" => "123"]);
+                    echo json_encode($this->gateway->get_all());
                     break;
             }
         }
