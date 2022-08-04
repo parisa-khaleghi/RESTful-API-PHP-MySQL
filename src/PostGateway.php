fdd8e21384e342c52777a8146a8eae0ec0ab310f
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 20:15:15 +0300

    Feat: add get all method

---
+++ src/PostGateway.php
@@ -0,0 +1,29 @@
+<?php
+
+    class PostGateway
+    {
+        private PDO $conn;
+        private string $table;
+
+        public function __construct(Database $database)
+        {
+            $this->conn = $database->get_connection();
+            $this->table = "posts";
+        }
+
+        public function get_all(): array
+        {
+            $sql = "SELECT * FROM ".$this->table;
+
+            $stmt = $this->conn->query($sql);
+
+            $data = [];
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $data = $row;
+            }
+
+            return $data;
+        }
+    }
+
+?>
