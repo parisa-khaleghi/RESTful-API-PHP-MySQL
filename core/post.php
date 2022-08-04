032e7cb6689840b72b5c4d74f73d61595322258b
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 13:15:03 +0300

    Feat: Add update function for PUT method

--- core/post.php
+++ core/post.php
@@ -94,5 +94,33 @@
             printf('Errro %s.\n', $stmt->error);
             return false;
         }
+        //update post function
+        public function update(){
+            //create query
+            $query = 'UPDATE '.$this->table.' 
+                      SET title= :title, body= :body, author= :author, category_id= :category_id
+                      WHERE id = :id';
+            //prepare statement
+            $stmt = $this->conn->prepare($query);
+            //clean data
+            $this->title         = htmlspecialchars(strip_tags($this->title));
+            $this->body          = htmlspecialchars(strip_tags($this->body));
+            $this->author        = htmlspecialchars(strip_tags($this->author));
+            $this->category_id   = htmlspecialchars(strip_tags($this->category_id));
+            $this->id   = htmlspecialchars(strip_tags($this->id));
+            //binding all parameters
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':body', $this->body);
+            $stmt->bindParam(':author', $this->author);
+            $stmt->bindParam(':category_id', $this->category_id);
+            $stmt->bindParam(':id', $this->id);
+            //execute query
+            if($stmt->execute()){
+                return true;
+            }
+            //print if something goes wrong
+            printf('Errro %s.\n', $stmt->error);
+            return false;
+        }
     }
 ?>
