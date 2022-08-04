508574d0fa32e8bcbfa016f1a0144ab058c8bb02
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-04 12:39:48 +0300

    Feat: Add create function for POST method

--- core/post.php
+++ core/post.php
@@ -71,4 +71,30 @@
             $this->category_name = $row['category_name'];
         }
     }
+    
+    public function create(){
+        //create query
+        $query = 'INSERT INTO '.$this->table.' SET title= :title, body= :body, author= :author, category_id= :category_id';
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
+        //clean data
+        $this->title         = htmlspecialchars(strip_tags($this->title));
+        $this->body          = htmlspecialchars(strip_tags($this->body));
+        $this->author        = htmlspecialchars(strip_tags($this->author));
+        $this->category_id   = htmlspecialchars(strip_tags($this->category_id));
+        //binding all parameters
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+        //execute query
+        if($stmt->execute()){
+            return true;
+        }
+        //print if something goes wrong
+        printf('Errro %s.\n', $stmt->error);
+        return false;
+    }
+
+
 ?>
